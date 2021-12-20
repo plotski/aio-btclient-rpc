3146cdabf358731c383c0a2d7e58a90745848b3e
plotski <plotski@example.org>
2021-12-20 13:18:43 +0100

    Add module: _errors

---
+++ aiobtclientrpc/_errors.py
@@ -0,0 +1,35 @@
+import re
+
+
+class Error(Exception):
+    """Base class for all exceptions raised by this package"""
+
+
+class RPCError(Error):
+    """Miscommunication with the RPC server, e.g. unknown method"""
+
+
+class ConnectionError(Error):
+    """Failed connect to the client, e.g. because it isn't running"""
+
+    def __init__(self, msg):
+        # python_socks.ProxyConnectionError provides ugly errors messages,
+        # e.g. "Could not connect to proxy localhost:1337 [None]".
+        msg = re.sub(r'\s+\[.*?\]$', '', str(msg))
+        super().__init__(msg)
+
+
+class TimeoutError(Error):
+    """Timeout for sending request and reading response"""
+
+
+class AuthenticationError(Error):
+    """Failed to prove identity"""
+
+
+class ValueError(Error, ValueError):
+    """
+    Invalid value (e.g. port 65536)
+
+    This is a subclass of :class:`Error` and :class:`ValueError`.
+    """
