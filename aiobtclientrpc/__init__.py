0339ba40573f97a82fd278be4e0ec5d706b30c58
plotski <plotski@example.org>
2021-12-21 16:48:33 +0100

    Change project description

--- aiobtclientrpc/__init__.py
+++ aiobtclientrpc/__init__.py
@@ -1,5 +1,5 @@
 __project_name__ = 'aio-btclient-rpc'
-__description__ = 'Asynchronous RPC interfaces for BitTorrent clients'
+__description__ = 'Asynchronous low-level communication with BitTorrent clients'
 __homepage__ = 'https://github.com/plotski/aio-btclient-rpc'
 __version__ = '0.0.0'
 __author__ = 'plotski'
