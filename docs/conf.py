580aa81a4779085133340a495cc633e77290a542
plotski <plotski@example.org>
2021-12-21 17:01:41 +0100

    Add docs

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
+
+autosummary_generate = True
+autosummary_imported_members = True
+
+html_show_sourcelink = False  # Don't show links to rST code
+
+templates_path = ['_templates']
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+}
---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+aiobtclientrpc
+==============
+
+.. include:: /../README.rst
+
+Reference
+---------
+
+.. autosummary::
+   :toctree:
+   :template: autosummary-template.rst
+   :nosignatures:
+
+   aiobtclientrpc
