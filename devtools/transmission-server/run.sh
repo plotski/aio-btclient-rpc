0f30d4fb79d26b9202cf4940ea13919aba8bf2f8
plotski <plotski@example.org>
2022-01-28 12:06:53 +0100

    Add devtools/transmission-server

---
+++ devtools/transmission-server/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+homedir="$(realpath "$(dirname "$0")")"
+workdir=/tmp/rtorrent
+rpc_port=5000
+source "$(dirname "$homedir")/auth"
+
+mkdir -p "$workdir"
+
+transmission-daemon --log-debug --foreground \
+                    --auth --username "$username" --password "$password" \
+                    --config-dir "$workdir/config/" \
+                    --download-dir "$workdir/downloads/" \
+                    --rpc-bind-address 127.0.0.1 --port "$rpc_port" \
+                    --peerport 54321 --no-portmap --no-dht \
+                    --paused
