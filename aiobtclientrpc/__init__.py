483105572f50a21f6e2480914fc169737757dfeb
plotski <plotski@example.org>
2022-06-14 15:03:03 +0200

    Bump version

--- aiobtclientrpc/__init__.py
+++ aiobtclientrpc/__init__.py
@@ -5,7 +5,7 @@ Asynchronous low-level communication with BitTorrent clients
 __project_name__ = 'aio-btclient-rpc'
 __description__ = 'Asynchronous low-level communication with BitTorrent clients'
 __homepage__ = 'https://github.com/plotski/aio-btclient-rpc'
-__version__ = '1.0.0'
+__version__ = '1.1.0alpha'
 __author__ = 'plotski'
 __author_email__ = 'plotski@example.org'
 
