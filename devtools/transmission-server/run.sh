53d16381f71fe945bd02d90d16aab93bf4a2c083
plotski <plotski@example.org>
2022-06-30 12:43:46 +0200

    devtools/transmission-server: Add --ui argument

--- devtools/transmission-server/run.sh
+++ devtools/transmission-server/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-set -o nounset   # Don't allow unset variables
-set -o errexit   # Exit if any command fails
+if [ "$1" = "--ui" ]; then
+    run_ui=true
+else
+    run_ui=false
+fi
 
 homedir="$(realpath "$(dirname "$0")")"
 workdir=/tmp/transmission.aiobtclientrpc
@@ -10,10 +13,29 @@ source "$(dirname "$homedir")/auth"
 
 mkdir -p "$workdir"
 
-transmission-daemon --log-debug --foreground \
-                    --auth --username "$username" --password "$password" \
-                    --config-dir "$workdir/config/" \
-                    --download-dir "$workdir/downloads/" \
-                    --rpc-bind-address 127.0.0.1 --port "$rpc_port" \
-                    --peerport 54321 --no-portmap --no-dht \
-                    --paused
+
+function run_transmission_daemon() {
+    transmission-daemon \
+        "$@" \
+        --pid-file "$workdir/pid" \
+        --config-dir "$workdir/config/" \
+        --download-dir "$workdir/downloads/" \
+        --auth --username "$username" --password "$password" \
+        --rpc-bind-address 127.0.0.1 --port "$rpc_port" \
+        --peerport 54321 --no-portmap --no-dht \
+        --paused
+}
+
+
+if [ "$run_ui" = "true" ]; then
+    run_transmission_daemon &
+    stig \
+        --no-rc-file \
+        set connect.url fnark:fnorkfnork@localhost:5000 \
+        and set tui.poll 1 \
+        and tab -C \
+        and ls
+    kill "$(cat "$workdir/pid")"
+else
+    run_transmission_daemon --log-debug --foreground
+fi
